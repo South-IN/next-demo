8d5ce78598b7e93a556e2284280b228b9fd7de0a
Saurav Singh <sauravsingh69420@gmail.com>
2024-08-31 20:02:54 +0530

    feat:instructors section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import Hero from "@/components/Hero";
+import Instructors from "@/components/Instructors";
 import ReviewCards from "@/components/ReviewCards";
 import Webinars from "@/components/Webinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
@@ -12,6 +13,7 @@ export default function Home() {
       <WhyChooseUs />
       <ReviewCards />
       <Webinars />
+      <Instructors />
     </main>
   );
 }
