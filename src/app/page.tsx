325fef1c85cc0cd360dac0a087d8c529c138f660
Saurav Singh <sauravsingh69420@gmail.com>
2024-09-01 08:40:12 +0530

    feat:footer section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Instructors from "@/components/Instructors";
 import ReviewCards from "@/components/ReviewCards";
@@ -14,6 +15,7 @@ export default function Home() {
       <ReviewCards />
       <Webinars />
       <Instructors />
+      <Footer />
     </main>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,66 @@
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <div className="bg-white text-black dark:bg-black dark:text-gray-400 py-10">
+      <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-4 sm:px-6 lg:px-8">
+        <div>
+          <h2 className="font-bold text-xl text-black dark:text-white">
+            About us
+          </h2>
+          <p className="mt-4">
+            We are a dedicated team of music educators and enthusiasts committed
+            to providing high-quality music education for all levels. Whether
+            you're just starting out or looking to refine your skills, our
+            courses are designed to help you reach your full potential.
+          </p>
+        </div>
+        <div>
+          <h2 className="font-bold text-xl text-black dark:text-white">
+            Contact us
+          </h2>
+          <p className="mt-4">
+            Have questions or need assistance? We're here to help!
+          </p>
+          <ul>
+            <li>Email: support@yourmusicwebsite.com</li>
+            <li>Phone: +1 (123) 456-7890</li>
+            <li>Address: 123 Music Lane, Suite 100, Your City, Your Country</li>
+          </ul>
+        </div>
+        <div>
+          <h2 className="font-bold text-xl text-black dark:text-white">
+            Follow us
+          </h2>
+          <p className="mt-4">
+            Stay connected and follow us on social media for the latest updates,
+            news, and exclusive content!
+          </p>
+          <ul>
+            <li>
+              <Link href={"/"}>Facebook</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Twitter</Link>
+            </li>
+            <li>
+              <Link href={"/"}>Instagram</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2 className="font-bold text-xl text-black dark:text-white">
+            Newsletter
+          </h2>
+          <p className="mt-4">
+            Subscribe to our newsletter and stay updated with the latest
+            courses, webinars, and special offers.
+          </p>
+          <Link href={"/"}>Subscribe Now</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
